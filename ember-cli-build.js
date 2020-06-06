d267bb0bd22d2ae87e2a010449dbf650fcb6a6e1
Tom Kraak <thomas.kraak@verizonmedia.com>
2020-06-06 15:06:50 -0400

    install and configure tailwindcss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,39 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { join } = require('path');
+const { WatchedDir } = require('broccoli-source');
+const compilePostCSS = require('broccoli-postcss-single');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    // Add options here
+  const app = new EmberApp(defaults, {
+    // other settings
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  const tailwindBase = join(__dirname, 'app', 'tailwind');
+  const plugins = [require('tailwindcss')(join(tailwindBase, 'config.js'))];
 
-  return app.toTree();
+  if (EmberApp.env() === 'production') {
+    plugins.push({
+      module: require('@fullhuman/postcss-purgecss'),
+      options: {
+        content: [
+          join(__dirname, 'app', 'index.html'),
+          join(__dirname, 'app', '**', '*.js'),
+          join(__dirname, 'app', '**', '*.hbs')
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        whitelistPatterns: [/^_/]
+      }
+    });
+  }
+
+  const tailwindNode = compilePostCSS(
+    new WatchedDir(tailwindBase),
+    'index.css',
+    'assets/tailwind.css',
+    { cacheInclude: [/.*\.(css|js)$/], plugins }
+  );
+
+  return app.toTree([tailwindNode]);
 };
